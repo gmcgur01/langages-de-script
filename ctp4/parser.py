45a1711f1b2da86e8e3627bd9bf5d65aae755113
gmcgur01 <George.McGurkin@tufts.edu>
2023-02-09 18:43:23 +0100

    ex6

--- ctp4/parser.py
+++ ctp4/parser.py
@@ -16,8 +16,7 @@ def main():
 
 def parse(file):
 
-    file.readline()
-    header = file.readline().strip()
+    header = next_line(file)
 
     weighted = False
     directed = False
@@ -27,22 +26,98 @@ def parse(file):
     else:
         components = header.split()
 
-        if len(components) > 2:
+        comp_len = len(components)
+
+        if comp_len < 2 or comp_len > 3:
+            raise SyntaxError("Improperly formatted header")
+        
+        if comp_len == 3:
             weighted = True
+
+        if components[1] != "oriente" and components[1] != "non-oriente":
+            raise SyntaxError("Improperly formatted header")
 
         if components[1] == "oriente":
             directed = True
 
-    file.readline()
-    nodes = file.readline().split()
+    if next_line(file).strip() != "Sommets:":
+        raise SyntaxError("Sommets keyword not found")
 
-    graphe = {node : {} for node in nodes}
+    nodes = next_line(file).split()
 
-    file.readline()
+    graphe = {node : set() for node in nodes}
 
-    
+    if next_line(file).strip() != "Transitions:":
+        raise SyntaxError("Transitions keyword not found")
+
+    while True:
+        curr_edge = next_line(file).strip()
+        if curr_edge == "":
+            break
+        parse_edge(curr_edge, graphe, weighted, directed)
 
     return graphe
+
+def next_line(file):
+    line = file.readline()
+    while line != "" and line[0] == "%":
+        line = file.readline()
+    return line
+
+def parse_edge(curr_edge, graphe, weighted, directed):
+    if directed and not weighted:
+
+        components = curr_edge.split()
+        if len(components) != 3 or components[1] != "-->":
+            raise SyntaxError("Improperly formatted edge")
+        if components[0] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        if components[2] not in graphe:
+            raise SyntaxError(f"{components[2]}: No such node exits")
+        graphe[components[0]].add(components[2])
+
+    if directed and weighted:
+
+        components = curr_edge.split()
+        if len(components) != 5 or components[1] != "--" or components[3] != "-->":
+            raise SyntaxError("Improperly formatted edge")
+        if components[0] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        if components[4] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        try:
+            weight = float(components[2])
+        except ValueError:
+            raise SyntaxError(f"{components[2]}: Invalid edge weight")
+        graphe[components[0]].add((components[4], weight))
+        
+    if not directed and not weighted:
+
+        components = curr_edge.split()
+        if len(components) != 3 or components[1] != "--":
+            raise SyntaxError("Improperly formatted edge")
+        if components[0] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        if components[2] not in graphe:
+            raise SyntaxError(f"{components[2]}: No such node exits")
+        graphe[components[0]].add(components[2])
+        graphe[components[2]].add(components[0])
+
+    if not directed and weighted:
+
+        components = curr_edge.split()
+        if len(components) != 5 or components[1] != "--" or components[3] != "--":
+            raise SyntaxError("Improperly formatted edge")
+        if components[0] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        if components[4] not in graphe:
+            raise SyntaxError(f"{components[0]}: No such node exits")
+        try:
+            weight = float(components[2])
+        except ValueError:
+            raise SyntaxError(f"{components[2]}: Invalid edge weight")
+        graphe[components[0]].add((components[4], weight))
+        graphe[components[4]].add((components[0], weight))
 
 if __name__ == "__main__":
     main()
